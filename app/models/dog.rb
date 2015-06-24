55f1f541bf938d640eb9268cad440ee670381c05
Sia <siakaramalegos@gmail.com>
2015-06-23 19:35:26 -0500

    Made papercli work

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,4 +3,11 @@ class Dog < ActiveRecord::Base
   belongs_to :breed
 
   validates :name, :owner_id, presence: true
+
+  has_attached_file :avatar,
+    :styles => {
+      :medium => "300x300#",
+      :thumb => "75x75#" },
+    :default_url => "/images/:style_missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
